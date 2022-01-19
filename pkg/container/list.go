d1d57c0fcb478ae2df3aac61e475004dda653145
Alex Walker <amlwwalker@gmail.com>
2022-01-19 13:32:37 +0000

    gas pump api init

---
+++ pkg/container/list.go
@@ -0,0 +1,27 @@
+package container
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"github.com/amlwwalker/gaspump-api/pkg/wallet"
+
+	"github.com/nspcc-dev/neofs-sdk-go/client"
+	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
+)
+
+func List(ctx context.Context, cli *client.Client, key *ecdsa.PrivateKey) ([]*cid.ID, error) {
+	// ListContainers method requires Owner ID.
+	// OwnerID is a binary representation of wallet address.
+	ownerID, err := wallet.OwnerIDFromPrivateKey(key)
+	if err != nil {
+		return nil, fmt.Errorf("can't retrieve owner ID: %w", err)
+	}
+	response, err := cli.ListContainers(ctx, ownerID)
+	if err != nil {
+		return nil, fmt.Errorf("can't list container: %w", err)
+	}
+
+	return response.IDList(), nil
+}
+
