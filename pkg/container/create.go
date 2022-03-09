63e7350925f9d740c5ceb6465f6d98362bb90e53
Alex Walker <amlwwalker@gmail.com>
2022-03-09 22:12:04 +0000

    removing timestamp from container creation

--- pkg/container/create.go
+++ pkg/container/create.go
@@ -4,9 +4,8 @@ import (
 	"context"
 	"crypto/ecdsa"
 	"fmt"
+
 	"github.com/configwizard/gaspump-api/pkg/wallet"
-	"strconv"
-	"time"
 
 	"github.com/nspcc-dev/neofs-sdk-go/acl"
 	"github.com/nspcc-dev/neofs-sdk-go/client"
@@ -25,11 +24,11 @@ func Create(ctx context.Context, cli *client.Client, key *ecdsa.PrivateKey, plac
 	// Read more about placement policy syntax in specification:
 	// https://github.com/nspcc-dev/neofs-spec/blob/master/01-arch/02-policy.md
 	// https://github.com/nspcc-dev/neofs-api/blob/master/netmap/types.proto
-//	const placementPolicy = `REP 2 IN X
-//CBF 2
-//SELECT 2 FROM * AS X
-//`
-//	customACL := acl.EACLReadOnlyBasicRule
+	//	const placementPolicy = `REP 2 IN X
+	//CBF 2
+	//SELECT 2 FROM * AS X
+	//`
+	//	customACL := acl.EACLReadOnlyBasicRule
 	containerPolicy, err := policy.Parse(placementPolicy)
 	if err != nil {
 		return nil, fmt.Errorf("can't parse placement policy: %w", err)
@@ -49,12 +48,11 @@ func Create(ctx context.Context, cli *client.Client, key *ecdsa.PrivateKey, plac
 		// https://github.com/nspcc-dev/neofs-spec/blob/master/01-arch/07-acl.md
 		container.WithCustomBasicACL(customACL),
 		// Attributes are key:value string pairs they are always optional
-		container.WithAttribute(
-			container.AttributeTimestamp,
-			strconv.FormatInt(time.Now().Unix(), 10),
-		),
+		//	container.WithAttribute(
+		//		container.AttributeTimestamp,
+		//		strconv.FormatInt(time.Now().Unix(), 10),
+		//	),
 	)
-	cnr.SetAttributes(attributes)
 	response, err := cli.PutContainer(ctx, cnr)
 	if err != nil {
 		return nil, fmt.Errorf("can't create new container: %w", err)
